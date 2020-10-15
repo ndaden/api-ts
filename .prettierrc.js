441067ccb6f731fc557bdc5017b5f60ff91830c3
Admin <nabil.daden@gmail.com>
2020-10-15 16:52:40 +0200

    added eslint

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+/*module.exports = {
+  "useTabs": false,
+  "semi": true,
+  "trailingComma": "all",
+  "singleQuote": true,
+  "printWidth": 100,
+  "tabWidth": 2
+};*/
+
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 4,
+};
--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,4 @@ import express from 'express';
 const app = express();
 app.get('/', (req, res) => res.send('hello'));
 
-
 export default app;
